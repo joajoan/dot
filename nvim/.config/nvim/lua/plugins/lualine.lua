067a17a00681c97fe8fd6161e06c0c5e05b9aecc
Johan Brandby <j@brandby.se>
2024-09-29 13:41:59 +0200

    feat: add NeoVim config

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { 
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function() 
+    require("lualine").setup({
+      options = { 
+	theme = "dracula-nvim",
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+      },
+    })
+  end
+}
