9dde60b4530d8509b7557039d487dcb9bf6603b1
Johan Brandby <j@brandby.se>
2024-09-30 08:50:43 +0200

    feat: merge neoscroll config from workstation

--- nvim/.config/nvim/lua/plugins/neoscroll.lua
+++ nvim/.config/nvim/lua/plugins/neoscroll.lua
@@ -1,6 +1,23 @@
 return {
   "karb94/neoscroll.nvim",
   config = function ()
-    require('neoscroll').setup({})
+    require('neoscroll').setup({
+      mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb' },
+      easing='sine',
+    })
+    local neoscroll = require('neoscroll')
+    local keymap = {
+      ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 180 }) end;
+      ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 180 }) end;
+      ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 320 }) end;
+      ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 320 }) end;
+      ["zt"] = function() neoscroll.zt({ half_win_duration = 180 }) end;
+      ["zz"] = function() neoscroll.zz({ half_win_duration = 120 }) end;
+      ["zb"] = function() neoscroll.zb({ half_win_duration = 180 }) end;
+    }
+    local modes = { 'n', 'v', 'x' }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
   end
 }
