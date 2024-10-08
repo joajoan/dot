751dfd585be39255790a00898c67f901ad5f930b
Johan Brandby <j@brandby.se>
2024-10-08 21:28:41 +0200

    feat(nvim): add vim-illuminate plugin

---
+++ nvim/.config/nvim/lua/plugins/vim-illuminate.lua
@@ -0,0 +1,13 @@
+return {
+  "RRethy/vim-illuminate",
+  event = { "BufReadPost", "BufNewFile" },  -- Load when opening a file
+  config = function()
+    require('illuminate').configure({
+      delay = 480,
+      modes_denylist = { 's' },
+    })
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3c3836" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3c3836" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3c3836" })
+  end,
+}
