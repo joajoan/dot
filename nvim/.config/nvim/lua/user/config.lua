18145711388999a37cbcfe8bb0e08229fa4e86a8
Johan Brandby <j@brandby.se>
2024-10-08 20:31:52 +0200

    refactor(nvim): add comments for readability

--- nvim/.config/nvim/lua/user/config.lua
+++ nvim/.config/nvim/lua/user/config.lua
@@ -1,34 +1,48 @@
+-- Leader key.
 vim.g.mapleader = ' '
 
+-- Line numbering.
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Sensible indenting.
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- Disable line wrapping.
 vim.opt.wrap = false
 
+-- Improve undo experience.
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 
+-- Search settings.
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- True color support.
 vim.opt.termguicolors = true
 
+-- Improved scrolling.
 vim.opt.scrolloff = 8
 
+-- Faster responsiveness.
 vim.opt.updatetime = 100
+
+-- Always show signs.
 vim.opt.signcolumn = 'number'
+
+-- Enable special filename characters.
 vim.opt.isfname:append('@-@')
 
+-- Hide mode indicator.
 vim.opt.showmode = false
 
+-- Enable spelling.
 vim.opt.spell = true
 vim.opt.spelllang = {'en_us', 'sv'}
