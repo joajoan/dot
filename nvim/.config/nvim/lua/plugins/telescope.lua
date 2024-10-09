586b5f896b53e58214bd05efb7253df428793fb1
Johan Brandby <j@brandby.se>
2024-10-09 10:42:39 +0200

    feat(nvim): add telescope-fzf-native plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,30 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function ()
-		require('telescope').setup({
-			defaults = {
-				file_ignore_patterns = { '**/.git/' }
-			},
-			pickers = {
-				find_files = { hidden = true }
-			}
-		})
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  config = function ()
+    local telescope = require('telescope')
+    telescope.setup({
+      defaults = {
+        file_ignore_patterns = { '**/.git/' }
+      },
+      pickers = {
+        find_files = { hidden = true }
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      },
+    })
+    telescope.load_extension('fzf')
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-	 end
+  end
 }
