727236241bca16c11410146962654e440cd8d041
Johan Brandby <j@brandby.se>
2024-11-07 13:51:10 +0100

    feat(nvim): use bubble theme for lualine plugin

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,21 +1,81 @@
+local function create_colors()
+  return {
+    primary = { bg = '#000000', fg = '#F8F8F2' },
+    normal = {
+      black   = '#262626',
+      red     = '#E64747',
+      green   = '#42E66C',
+      yellow  = '#E4F34A',
+      blue    = '#9B6BDF',
+      cyan    = '#75D7EC',
+    },
+    bright = { black = '#7A7A7A', white = '#FFFFFF' },
+  }
+end
+
+local function create_theme(colors)
+  return {
+    normal = {
+      a = { fg = colors.primary.bg, bg = colors.normal.blue, gui = 'bold' },
+      b = { fg = colors.primary.fg, bg = colors.normal.black },
+      c = { fg = colors.primary.fg, bg = nil },
+    },
+    insert = {
+      a = { fg = colors.primary.bg, bg = colors.normal.green, gui = 'bold' },
+    },
+    visual = {
+      a = { fg = colors.primary.bg, bg = colors.normal.cyan, gui = 'bold' },
+    },
+    replace = {
+      a = { fg = colors.primary.bg, bg = colors.normal.red, gui = 'bold' },
+    },
+    command = {
+      a = { fg = colors.primary.bg, bg = colors.normal.yellow, gui = 'bold' },
+    },
+    inactive = {
+      a = { fg = colors.bright.white, bg = colors.bright.black },
+      b = { fg = colors.bright.white, bg = colors.bright.black },
+      c = { fg = colors.bright.white, bg = nil },
+    },
+  }
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local colors = create_colors()
+    local theme = create_theme(colors)
     require("lualine").setup({
       options = {
-	theme = "dracula-nvim",
+        theme = theme,
+        component_separators = '',
+        section_separators = { left = '', right = '' },
       },
       sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch'},
-        lualine_c = {'filename'},
-        lualine_x = {'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'},
+        lualine_a = { { 'mode', separator = { left = '', right = ''}, right_padding = 2 } },
+        lualine_b = { 'branch', 'filename' },
+        lualine_c = {
+          '%=', --[[ add your center compoentnts here in place of this comment ]]
+        },
+        lualine_x = {},
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+          { 'location', separator = { left = '', right = '' }, left_padding = 2 },
+        },
       },
+      inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+      tabline = {},
+      extensions = {},
     })
   end
 }
