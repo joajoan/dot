d5cb76ed68134c06c86899c07ebfc0f5219eca83
Johan Brandby <j@brandby.se>
2024-11-22 15:04:08 +0100

    feat(nvim): remove mason-based setup for nvim-lspconfig plugin

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,22 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = { "williamboman/mason-lspconfig.nvim" },
   config = function()
-    local mason = require("mason-lspconfig")
     local lspconfig = require("lspconfig")
-    mason.setup({
-      ensure_installed = {
-        "pyright",
-        "lua_ls",
-        "rust_analyzer",
-        "sqlls",
-      },
-    })
-    mason.setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({})
-      end,
-    })
     lspconfig.lua_ls.setup({
       settings = {
         Lua = {
