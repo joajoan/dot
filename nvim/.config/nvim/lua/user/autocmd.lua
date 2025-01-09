ee92327751d561a76ccea4db6f0eb8fdf182191e
Johan Brandby <j@brandby.se>
2025-01-09 18:51:09 +0100

    feat(nvim): add keybinding for LSP-based file reformatting

--- nvim/.config/nvim/lua/user/autocmd.lua
+++ nvim/.config/nvim/lua/user/autocmd.lua
@@ -22,6 +22,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>vf", vim.lsp.buf.format, opts)
     vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
     vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
