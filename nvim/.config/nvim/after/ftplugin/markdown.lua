cfc8eebf007ade531ded48ae3dba676d2a20356d
Johan Brandby <j@brandby.se>
2025-02-07 11:05:55 +0100

    refactor(nvim): add descriptive comments to markdown-based lua config

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,8 @@
+-- Set tab size
 vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 
+-- Set line wrapping
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
