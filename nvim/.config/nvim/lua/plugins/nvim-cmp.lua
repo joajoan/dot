414ccf62f6307abb039c6aab430522cd31f14cda
Johan Brandby <j@brandby.se>
2025-05-01 21:27:46 +0200

    feat(nvim): remove code expansion plugins

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -5,20 +5,10 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
-    "saadparwaiz1/cmp_luasnip",
-    "L3MON4D3/LuaSnip",
-    "rafamadriz/friendly-snippets",
   },
   config = function()
     local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    require("luasnip.loaders.from_vscode").lazy_load()
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end
-      },
       mapping = cmp.mapping.preset.insert({
         ["<Tab>"] = cmp.mapping.select_next_item(),
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
@@ -30,7 +20,6 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp", keyword_length = 0 },
-        { name = "luasnip", keyword_length = 0 },
       }, {
         { name = "buffer" },
         { name = "path" },
