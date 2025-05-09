84029b3464b5045c76b72add811c05b0660a1f4d
Johan Brandby <j@brandby.se>
2025-05-09 08:57:07 +0200

    feat(nvim): trigger oil plugin on enter

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = false,
+  event = "VimEnter",
   opts = {
     default_file_explorer = true,
     view_options = { show_hidden = true },
