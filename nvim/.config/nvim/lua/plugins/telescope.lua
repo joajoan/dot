40d965fa034d5e3346ca7f9c844576aea6630b1d
Johan Brandby <j@brandby.se>
2025-05-11 16:36:11 +0200

    refactor(nvim): initialize telescope plugin using package manager

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,50 +1,48 @@
 return {
   "nvim-telescope/telescope.nvim",
-  event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
-  config = function ()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    telescope.setup({
-      defaults = {
-        file_ignore_patterns = { "%.git/" },
-        mappings = {
-          n = { ["<C-c>"] = actions.close },
-          i = {
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-          },
+  keys = {
+    { "<leader>ff", "<CMD>Telescope find_files<CR>" },
+    { "<leader>fg", "<CMD>Telescope live_grep<CR>" },
+    { "<leader>fb", "<CMD>Telescope buffers<CR>" },
+  },
+  opts = {
+    defaults = {
+      file_ignore_patterns = { "%.git/" },
+      mappings = {
+        n = { ["<C-c>"] = "close" },
+        i = {
+          ["<C-j>"] = "move_selection_next",
+          ["<C-k>"] = "move_selection_previous",
         },
       },
-      pickers = {
-        find_files = { hidden = true },
-        live_grep = {
-          additional_args = { "--hidden" },
-        },
+    },
+    pickers = {
+      find_files = { hidden = true },
+      live_grep = {
+        additional_args = { "--hidden" },
       },
-      extensions = {
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = "smart_case",
-        }
-      },
-    })
-    telescope.load_extension("fzf")
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fs", builtin.git_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fb", function()
-      builtin.buffers({
+      buffers = {
         ignore_current_buffer = true,
         sort_lastused = true,
         sort_mru = true,
-      })
-    end, {})
-  end
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      }
+    },
+  },
+  config = function (_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("fzf")
+  end,
 }
