fc931353604854c222a13349418cc92e4f39fa3f
Johan Brandby <j@brandby.se>
2025-05-15 07:49:59 +0200

    refactor(nvim): change configuration method of nvim-treesitter plugin

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,56 +6,57 @@ return {
   },
   event = { "BufNewFile", "BufReadPost" },
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "lua",
-        "python",
-        "sql",
-        "rust",
+  opts = {
+    ensure_installed = {
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "lua",
+      "python",
+      "sql",
+      "rust",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    spell = { enable = true },
+    indent = { enable = false },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
       },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
+    },
+    textobjects = {
+      select = {
         enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      spell = { enable = true },
-      indent = { enable = false },
-      incremental_selection = {
-        enable = true,
+        lookahead = true,
         keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
         },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-          },
-          selection_modes = {
-            ["@block.outer"] = "V",
-            ["@class.outer"] = "V",
-            ["@function.outer"] = "V",
-          },
-          include_surrounding_whitespace = true,
+        selection_modes = {
+          ["@block.outer"] = "V",
+          ["@class.outer"] = "V",
+          ["@function.outer"] = "V",
         },
+        include_surrounding_whitespace = true,
       },
-    })
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
   end
 }
