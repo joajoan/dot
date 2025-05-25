1d35ecbbfc018818f93cd1c7592f51f724907105
Johan Brandby <j@brandby.se>
2025-05-25 12:00:23 +0200

    refactor(nvim): replace single with double quotes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,24 +1,24 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('config')
-require('lazy').setup('plugins', {
+require("config")
+require("lazy").setup("plugins", {
   change_detection = { notify = false },
   install = {
     colorscheme = { "dracula" },
   },
   ui = {
-    border = 'rounded',
-    title = ' Lazy ',
+    border = "rounded",
+    title = " Lazy ",
   },
 })
