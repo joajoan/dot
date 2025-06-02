86aa569a3c8293595318bb152b3ce2cbbdf478da
Johan Brandby <j@brandby.se>
2025-06-02 09:40:52 +0200

    refactor(nvim): reorder config table of oil plugin

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,11 +2,11 @@ return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "VimEnter",
+  keys = {
+    { "-", "<CMD>Oil<CR>" },
+  },
   opts = {
     default_file_explorer = true,
     view_options = { show_hidden = true },
-  },
-  keys = {
-    { "-", "<CMD>Oil<CR>" },
   },
 }
