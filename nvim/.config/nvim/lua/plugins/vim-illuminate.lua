00b918031e49238759db694ffd567a1344d7da53
Johan Brandby <j@brandby.se>
2025-06-02 09:34:07 +0200

    refactor(nvim): move options out of config function for vim-illuminate plugin

--- nvim/.config/nvim/lua/plugins/vim-illuminate.lua
+++ nvim/.config/nvim/lua/plugins/vim-illuminate.lua
@@ -1,11 +1,12 @@
 return {
   "RRethy/vim-illuminate",
   event = { "BufNewFile", "BufReadPost" },
-  config = function()
-    require("illuminate").configure({
-      delay = 480,
-      modes_denylist = { "s" },
-    })
+  opts = {
+    delay = 480,
+    modes_denylist = { "s" },
+  },
+  config = function(_, opts)
+    require("illuminate").configure(opts)
     vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3c3836" })
     vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3c3836" })
     vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3c3836" })
