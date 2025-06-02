2161587ed905f536be8901cd2de36764f3439878
Johan Brandby <j@brandby.se>
2025-06-02 09:43:47 +0200

    feat(nvim): disable select plugins when initialized by VS Code

--- nvim/.config/nvim/lua/plugins/vim-illuminate.lua
+++ nvim/.config/nvim/lua/plugins/vim-illuminate.lua
@@ -1,13 +1,14 @@
 return {
   "RRethy/vim-illuminate",
   event = { "BufNewFile", "BufReadPost" },
+  cond = not vim.g.vscode,
   init = function ()
     vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3c3836" })
     vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3c3836" })
     vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3c3836" })
   end,
   opts = { delay = 250 },
-  config = function(_, opts)
+  config = function (_, opts)
     require("illuminate").configure(opts)
   end,
 }
