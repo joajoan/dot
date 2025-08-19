b555c4d9e290ed5133784ee5fdae60b968749920
Johan Brandby <j@brandby.se>
2025-08-19 08:46:22 +0200

    refactor(nvim): move highlight groups into init function for illuminate plugin

--- nvim/.config/nvim/lua/plugins/vim-illuminate.lua
+++ nvim/.config/nvim/lua/plugins/vim-illuminate.lua
@@ -1,14 +1,16 @@
 return {
   "RRethy/vim-illuminate",
   event = { "BufNewFile", "BufReadPost" },
+  init = function ()
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3c3836" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3c3836" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3c3836" })
+  end,
   opts = {
     delay = 480,
     modes_denylist = { "s" },
   },
   config = function(_, opts)
     require("illuminate").configure(opts)
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3c3836" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3c3836" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3c3836" })
   end,
 }
