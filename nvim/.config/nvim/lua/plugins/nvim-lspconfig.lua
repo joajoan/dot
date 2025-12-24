c883d715c7f94c81ecbefbff0d07ca0c48800ce4
Johan Brandby <j@brandby.se>
2025-12-24 13:21:58 +0100

    feat(nvim): add LSP on_attach callback for nvim-lspconfig plugin
    Add on_attach function to enable LSP auto-formatting if available.

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,15 @@
+local group = vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
+local function on_attach(_, buffer)
+  vim.api.nvim_clear_autocmds({ group = group, buffer = buffer })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = group,
+    buffer = buffer,
+    callback = function()
+      vim.lsp.buf.format({ async = false })
+    end
+  })
+end
+
 return {
   "neovim/nvim-lspconfig",
   event = { "BufNewFile", "BufReadPost" },
@@ -12,6 +24,7 @@ return {
       "rust_analyzer",
     })
     vim.lsp.config("lua_ls", {
+      on_attach = on_attach,
       settings = {
         Lua = {
           diagnostics = {
