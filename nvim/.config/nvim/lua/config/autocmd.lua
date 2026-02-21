0d49409a4b44f641cd5a463bf6c91eb7bab842cd
Johan Brandby <j@brandby.se>
2026-02-21 15:59:09 +0100

    feat(nvim): remove disable-hover auto-command for ruff LSP

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -27,20 +27,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
--- Disable hover capability from Ruff.
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("RuffLsp", { clear = true }),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client == nil then
-      return
-    end
-    if client.name == "ruff" then
-      client.server_capabilities.hoverProvider = false
-    end
-  end,
-})
-
 -- Set comment string for SQL files.
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "sql",
