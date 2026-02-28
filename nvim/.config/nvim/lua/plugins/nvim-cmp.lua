90c14193b1e2a05d80690c27ad03b8134ce3a8fb
Johan Brandby <j@brandby.se>
2026-02-28 17:49:37 +0100

    fix(nvim): enable default cmdline keymaps in nvim-cmp plugin

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -26,6 +26,7 @@ return {
       }),
     })
     cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
         { name = "path" },
       }, {
