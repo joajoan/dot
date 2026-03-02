20378d5f7e55a0f46e2ff12906ba4b9ad6013e23
Johan Brandby <j@brandby.se>
2026-03-02 21:07:05 +0100

    fix(nvim): override preset cmp highlight groups in dracula plugin

--- nvim/.config/nvim/lua/plugins/dracula.lua
+++ nvim/.config/nvim/lua/plugins/dracula.lua
@@ -6,6 +6,9 @@ return {
     colors = { bg = "#000000" },
     overrides = {
       CursorLine = { bg = "#1a1a1a" },
+      CmpItemAbbr = { fg = "#f8f8f2", bg = "NONE" },
+      CmpItemAbbrMatch = { fg = "#8be9fd", bg = "NONE", bold = true },
+      CmpItemAbbrMatchFuzzy = { fg = "#8be9fd", bg = "NONE", bold = true },
       MatchParen = { bg = "#44475A", underline = false },
     },
   },
