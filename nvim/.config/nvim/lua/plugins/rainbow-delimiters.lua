ff1d4a41b73b1f483cf6d134de8d3c79d42df4a6
Johan Brandby <j@brandby.se>
2026-03-08 09:40:51 +0100

    feat(nvim): remove custom highlighting in rainbow-delimiters.nvim plugin

--- nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
+++ nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -2,26 +2,8 @@ return {
   "HiPhish/rainbow-delimiters.nvim",
   event = { "BufNewFile", "BufReadPost" },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  init = function ()
-    vim.api.nvim_set_hl(0, "DelimiterCyan", { fg = "#80D7EA" })
-    vim.api.nvim_set_hl(0, "DelimiterPink", { fg = "#EB6FB7" })
-    vim.api.nvim_set_hl(0, "DelimiterGreen", { fg = "#4AE771" })
-    vim.api.nvim_set_hl(0, "DelimiterOrange", { fg = "#EBAA63" })
-    vim.api.nvim_set_hl(0, "DelimiterRed", { fg = "#EB4E4E" })
-    vim.api.nvim_set_hl(0, "DelimiterPurple", { fg = "#AE87E6" })
-    vim.api.nvim_set_hl(0, "DelimiterYellow", { fg = "#DEE781" })
-  end,
   opts = {
     priority = { [''] = 110 },
-    highlight = {
-      "DelimiterRed",
-      "DelimiterOrange",
-      "DelimiterPink",
-      "DelimiterCyan",
-      "DelimiterYellow",
-      "DelimiterPurple",
-      "DelimiterGreen",
-    },
   },
   main = "rainbow-delimiters.setup",
 }
