d090bddb9eed9b088ad3641680c57580c80d22bf
Johan Brandby <j@brandby.se>
2026-03-08 13:17:43 +0100

    fix(nvim): move enable to after config calls in nvim-lspconfig plugin

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,14 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufNewFile", "BufReadPost" },
   config = function()
-    vim.lsp.enable({
-      "lua_ls",
-      "sqruff",
-      "ts_ls",
-      "ruff",
-      "ty",
-      "rust_analyzer",
-    })
     vim.lsp.config("lua_ls", {
       settings = {
         Lua = {
@@ -32,6 +24,14 @@ return {
       on_attach = function(client)
         client.server_capabilities.hoverProvider = false
       end,
+    })
+    vim.lsp.enable({
+      "lua_ls",
+      "sqruff",
+      "ts_ls",
+      "ruff",
+      "ty",
+      "rust_analyzer",
     })
   end,
 }
