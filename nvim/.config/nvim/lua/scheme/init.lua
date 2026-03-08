a013282419afa1a006807ec793370fd30cc664ca
Johan Brandby <j@brandby.se>
2026-03-08 09:37:06 +0100

    feat(nvim): add load function to private colorscheme module

--- nvim/.config/nvim/lua/scheme/init.lua
+++ nvim/.config/nvim/lua/scheme/init.lua
@@ -1,7 +1,22 @@
 ---Private Dracula colorscheme.
 ---@class Scheme
+---@field load fun()
 local M = {}
 
 M.palette = require("scheme.palette")
+
+---Load the colorscheme into the current Neovim session.
+function M.load()
+  if vim.g.colors_name then
+    vim.cmd("hi clear")
+  end
+  if vim.fn.exists("syntax_on") == 1 then
+    vim.cmd("syntax reset")
+  end
+  vim.o.background = "dark"
+  vim.o.termguicolors = true
+  vim.g.colors_name = "scheme"
+  require("scheme.groups").setup(M.palette)
+end
 
 return M
